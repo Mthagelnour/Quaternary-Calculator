4d5c8fc0eb3d323f7d1ebf9934a8437e765828c8
Jacob Bishop <jacobbishop1000@gmail.com>
2022-09-06 10:09:40 -0400

    Added calculatorTest.java file Added tests for converting dec to quat and quat to dec Added JUnit dependencies for Gradle Added dummy returns for conversion functions to be implemented

--- src/main/java/model/calculator.java
+++ src/main/java/model/calculator.java
@@ -1,4 +1,13 @@
 package model;
 
 public class calculator {
+    public static String decToQuat(int decimal){
+        return ""; // I think we store quat numbers as strings so they aren't accidentally misinterpreted as ints (which use decimal)
+    }
+
+    public static int quatToDec(String quaternary){
+        return 0;
+    }
+
+
 }
