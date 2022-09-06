3035a18281adedd0ff013454159de3d0b68b3c70
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2022-09-06 12:55:41 -0400

    Add Controller.java for UI

---
+++ src/main/java/view/Controller.java
@@ -0,0 +1,49 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField textField;
+
+    public void button0Clicked(ActionEvent event) {
+        ;
+    }
+
+    public void button1Clicked(ActionEvent event) {
+        ;
+    }
+
+    public void button2Clicked(ActionEvent event) {
+        ;
+    }
+
+    public void button3Clicked(ActionEvent event) {
+        ;
+    }
+
+    public void calculate(ActionEvent event) {
+        ;
+    }
+
+    public void clearTextField(ActionEvent event) {
+        ;
+    }
+
+    public void addAction(ActionEvent event) {
+        ;
+    }
+
+    public void minusAction(ActionEvent event) {
+        ;
+    }
+
+    public void divideAction(ActionEvent event) {
+        ;
+    }
+
+    public void multiplicationAction(ActionEvent event) {
+        ;
+    }
+
+}
