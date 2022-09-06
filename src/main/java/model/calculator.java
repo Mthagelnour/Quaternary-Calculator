0ac47485d28841698e04add13b031cc592a4642e
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2022-09-06 17:14:49 -0400

    add conversion functions
    Signed-off-by: Jeremiah Deffendall <jddeffendal2@bsu.edu>

--- src/main/java/model/calculator.java
+++ src/main/java/model/calculator.java
@@ -1,13 +1,24 @@
 package model;
 
 public class calculator {
-    public static String decToQuat(int decimal){
-        return ""; // I think we store quat numbers as strings so they aren't accidentally misinterpreted as ints (which use decimal)
+
+    public int toDecimal(int quaternaryNumber) {
+        int num = 0;
+        int j = 0;
+        char[] digitList = quaternaryNumber.toString().toCharArray();
+        for (int i = digitList.length-1; i >= 0; i--){
+            num += Integer.parseInt(String.valueOf(digitList[i])) * Math.pow(4, j);
+            j++;
+        }
+        return num;
     }
-
-    public static int quatToDec(String quaternary){
-        return 0;
+    public int toQuaternary(int decimalNumber) {
+        String quaternary = "";
+        while (decimalNumber > 0) {
+            quaternary += (decimalNumber % 4);
+            decimalNumber /= 4;
+        }
+        StringBuilder reverseQuaternary = new StringBuilder(quaternary).reverse();
+        return Integer.parseInt(reverseQuaternary.toString()); //return the quaternary number as integer
     }
-
-
 }
